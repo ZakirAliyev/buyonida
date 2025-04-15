26f7440336d398c43db2b93422f6d342b50b5562
AnarZulfugarov <anarrz-af106@code.edu.az>
2025-04-15 18:39:31 +0400

    Buyonida

--- src/pages/MarketPages/index.jsx
+++ src/pages/MarketPages/index.jsx
@@ -2,13 +2,21 @@ import { Outlet } from "react-router-dom";
 import MarketNavbar from "../../components/MarketComponents/MarketNavbar/index.jsx";
 import MarketFooter from "../../components/MarketComponents/MarketFooter/index.jsx";
 import Cookies from "js-cookie";
-import { useGetStoreWithSectionsQuery } from "../../service/userApi.js";
+import {useGetStoreByNameQuery, useGetStoreWithSectionsQuery} from "../../service/userApi.js";
+import {useLocation} from "react-router";
 
 const MarketLayout = () => {
-    const id = Cookies.get('chooseMarket');
+    const location = useLocation();
+    const cleanedPath = location.pathname
+        .replace(/\//g, '')
+        .replace(/%20/g, ' ')
+        .replace(/@/, '');
+    const { data: getStoreByName } = useGetStoreByNameQuery(cleanedPath);
+    const store = getStoreByName?.data;
+    console.log(store);
+    const id = store?.id;
     const { data: getStoreWithSections, isLoading, isError } = useGetStoreWithSectionsQuery(id);
 
-    // Safely access data
     const sections = getStoreWithSections?.data?.sections || [];
     const palets = getStoreWithSections?.data?.palets || [];
     const selectedPaletId = getStoreWithSections?.data?.selectedPaletId;
