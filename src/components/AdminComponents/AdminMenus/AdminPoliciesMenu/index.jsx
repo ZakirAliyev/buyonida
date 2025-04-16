883a10414998654d0d67fbe0e141d679f82f95e0
zakiraliyev <aliyevzakir814@gmail.com>
2025-04-16 19:36:30 +0400

    BUYONIDA

--- src/components/AdminComponents/AdminMenus/AdminPoliciesMenu/index.jsx
+++ src/components/AdminComponents/AdminMenus/AdminPoliciesMenu/index.jsx
@@ -1,9 +1,117 @@
 import './index.scss'
+import {TbCreditCardRefund} from "react-icons/tb";
+import {IoLockOpenOutline} from "react-icons/io5";
+import {FiFileText} from "react-icons/fi";
+import {MdOutlineLocalShipping} from "react-icons/md";
+import {HiOutlineClipboardDocumentList} from "react-icons/hi2";
+import {BiChevronRight} from "react-icons/bi";
 
 function AdminPoliciesMenu() {
     return (
         <section id={"adminPoliciesMenu"}>
-            <h2>Admin general menu</h2>
+            <h2>Policies</h2>
+            <div className={"box wrapper"}>
+                <div className={"storeDetails"}>Written policies</div>
+                <div className={"boz"}>Policies are linked in the footer of checkout and can be added to your online store menu</div>
+                <div className={"box wrapper"} style={{
+                    padding: "16px",
+                }}>
+                    <div className={"padding"}>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <TbCreditCardRefund />
+                            Return and refund policy
+                        </div>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <div className={"policy"}>No policy yet</div>
+                            <BiChevronRight/>
+                        </div>
+                    </div>
+                    <div className={"line"}></div>
+                    <div className={"padding"}>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <IoLockOpenOutline />
+                            Privacy policy
+                        </div>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <div className={"policy"}>No policy yet</div>
+                            <BiChevronRight/>
+                        </div>
+                    </div>
+                    <div className={"line"}></div>
+                    <div className={"padding"}>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <FiFileText />
+                            Terms of service
+                        </div>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <div className={"policy"}>No policy yet</div>
+                            <BiChevronRight/>
+                        </div>
+                    </div>
+                    <div className={"line"}></div>
+                    <div className={"padding"}>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <MdOutlineLocalShipping />
+                            Shipping policy
+                        </div>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <div className={"policy"}>No policy yet</div>
+                            <BiChevronRight/>
+                        </div>
+                    </div>
+                    <div className={"line"}></div>
+                    <div className={"padding"}>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <HiOutlineClipboardDocumentList />
+                            Contact information
+                        </div>
+                        <div style={{
+                            display: "flex",
+                            alignItems: "center",
+                            gap: "16px",
+                        }}>
+                            <div className={"policy"}>No policy yet</div>
+                            <BiChevronRight/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 }
--- src/components/AdminComponents/AdminMenus/AdminShippingAndDeliveryMenu/index.jsx
+++ src/components/AdminComponents/AdminMenus/AdminShippingAndDeliveryMenu/index.jsx
@@ -1,7 +1,53 @@
-import './index.scss'
-import kapital from "../../../../assets/kapital.png";
+import './index.scss';
+import aze from "../../../../assets/aze.png";
+import { FiPlus } from "react-icons/fi";
+import { GoDotFill } from "react-icons/go";
+import { useState } from 'react';
 
 function AdminShippingAndDeliveryMenu() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [formData, setFormData] = useState({
+        rateType: 'flat',
+        customRateName: '',
+        customDescription: '',
+        price: '0.00',
+    });
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setIsModalOpen(false);
+        setFormData({
+            rateType: 'flat',
+            customRateName: '',
+            customDescription: '',
+            price: '0.00',
+        });
+    };
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleSetFree = () => {
+        setFormData((prev) => ({
+            ...prev,
+            price: '0.00',
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log("Form Data Submitted:", formData);
+        handleCloseModal();
+    };
+
     return (
         <section id={"adminShippingAndDeliveryMenu"}>
             <h2>Shipping and Delivery</h2>
@@ -18,18 +64,119 @@ function AdminShippingAndDeliveryMenu() {
                         Shipping zones
                     </div>
                     <div className={"olan"}>
-                        Full
+                        <img src={aze} alt={"Image"} />
+                        <div>Domestic</div>
+                        <GoDotFill style={{
+                            color: "#ccc",
+                            fontSize: "10px",
+                        }} />
+                        <div>Azerbaijan</div>
                     </div>
                     <div className={"line"}></div>
-                    <div className={"olan"}>
-                        Full
+                    <div className={"olan"} style={{
+                        justifyContent: "space-between"
+                    }}>
+                        Standart
+                        <div style={{
+                            background: '#AFFEBF',
+                            padding: "2px 24px",
+                            borderRadius: "15px",
+                        }}>Free</div>
                     </div>
                     <div className={"line"}></div>
-                    <div className={"olan"}>
-                        Full
+                    <div className={"olan"} onClick={handleOpenModal} style={{ cursor: 'pointer' }}>
+                        <FiPlus className="icon" />
+                        Add rate
                     </div>
                 </div>
             </div>
+
+            {isModalOpen && (
+                <div className="modal-overlay">
+                    <div className="modal-content">
+                        <div style={{
+                            width: "100%",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: 'center',
+                            background: "#F1F1F1",
+                        }}><h3>Add rate</h3></div>
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <label style={{
+                                    marginTop: '20px'
+                                }}>Rate type</label>
+                                <select
+                                    name="rateType"
+                                    value={formData.rateType}
+                                    onChange={handleChange}
+                                >
+                                    <option value="flat">Use flat rate</option>
+                                    <option value="custom">Custom</option>
+                                </select>
+                            </div>
+                            <div className="form-group">
+                                <label>Shipping rate</label>
+                                <select
+                                    name="rateType"
+                                    value={formData.rateType}
+                                    onChange={handleChange}
+                                >
+                                    <option value="custom">Custom</option>
+                                    <option value="flat">Use flat rate</option>
+                                </select>
+                            </div>
+                            <div className="form-group">
+                                <label>Custom rate name</label>
+                                <input
+                                    type="text"
+                                    name="customRateName"
+                                    value={formData.customRateName}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="form-group">
+                                <label>Custom delivery description (optional)</label>
+                                <textarea
+                                    name="customDescription"
+                                    value={formData.customDescription}
+                                    onChange={handleChange}
+                                />
+                            </div>
+                            <div className="form-group price-group">
+                                <div>
+                                    <label>Price</label>
+                                    <input
+                                        type="number"
+                                        name="price"
+                                        value={formData.price}
+                                        onChange={handleChange}
+                                        step="0.01"
+                                        min="0"
+                                    />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <span className="add-conditional">
+                                    Add conditional pricing
+                                </span>
+                            </div>
+                            <div className="modal-actions">
+                                <button
+                                    type="button"
+                                    className="cancel-button"
+                                    onClick={handleCloseModal}
+                                >
+                                    Cancel
+                                </button>
+                                <button type="submit" className="done-button">
+                                    Done
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
         </section>
     );
 }
